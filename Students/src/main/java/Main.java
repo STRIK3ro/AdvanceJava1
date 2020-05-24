b2a3786d85aff6b60690ac561981cf77a46226f4
STRIK3ro <STRIK3.2014@yahoo.com>
2020-05-24 10:42:12 +0300

    Student

--- Students/src/main/java/Main.java
+++ Students/src/main/java/Main.java
@@ -24,14 +24,14 @@ public class Main {
         }catch (BadGradesExeption e){
             System.out.println("Din pacate a aparut eroare : "+e.getMessage());
         }
-        System.out.println("Rezultatul admiterii la poli este " + student.admisPoli);
+        System.out.println("Rezultatul admiterii la poli este " + student.getAdmisPoli());
     //sa se creeze exeptia atunci cand notele sunt mai mari ce 10 dar si mai mici ca 1
     //sa se adauge urmatorul comportament prin intermediul unei interfete
     //sa se calculeze media doar la mate si fizica si sa se adauge un camp admis la facultatea de fizica
     //sa se calculeze media doar la mate si info si un camp admis la info
     //sa se adauge o exeptie atunci cand media este 10 cu mesaj
         try{
-            if(student.admisPoli==false){
+            if(student.getAdmisPoli()==false){
                 student.computeAvrageFizica();
 
             }
@@ -40,9 +40,9 @@ public class Main {
         }catch (BadGradesExeption e){
             System.out.println("Din pacate a aparut eroarea : "+e.getMessage());
         }
-        System.out.println("Rezultatul admiterii la fizica este :" +student.admisFizica);
+        System.out.println("Rezultatul admiterii la fizica este :" +student.getAdmisFizica());
         try{
-            if(student.admisFizica==false){
+            if(student.getAdmisFizica()==false){
                 student.computeAvrageInfo();
             }
         }catch (GradesOutOfReachExeption e){
@@ -50,9 +50,54 @@ public class Main {
         }catch (BadGradesExeption e){
             System.out.println("Din pacate a aparut eroarea :" +e.getMessage());
         }
-        System.out.println("Rezultatul admiterii la mate info este : "+student.admisMateInfo);
-        if(student.admisMateInfo==false){
+        System.out.println("Rezultatul admiterii la mate info este : "+student.getAdmisMateInfo());
+        if(student.getAdmisMateInfo()==false){
             System.out.println("Mai stai un an pe tusa somere");
+
+
         }
+        //Cum comparam studentii ?
+        //Dupa note
+        //Creem doi studenti
+
+        Student student_alex=new Student("Alex","Ion",8,7,9);
+        Student student_liviu=new Student("Liviu","Preda",5,7,6);
+        Student student_robert=new Student("Robert","Rotaru",10,10,10);
+
+        int medieA=(student_alex.getMathGrade()+student_alex.getPhizGrade()+student_alex.getInfoGrade())/3;
+        int medieL=(student_liviu.getMathGrade()+student_liviu.getPhizGrade()+student_liviu.getInfoGrade())/3;
+        if(medieA>medieL){
+            System.out.println("Media lui Alex este mai mare ca a lui Liviu");
+        }else{
+            System.out.println("Media lui Liviu este mai mare ca a lui Alex");
+        }
+
+            
+
+
+        if(student_alex.getMedie()>student_liviu.getMedie()){
+            System.out.println("Media lui Alex este mai mare ca a lui Liviu");
+        }else{
+            System.out.println("Media lui Liviu este mai mare ca a lui Alex");
+        }
+
+
+
+
+         int comparatie=student_alex.compareTo(student_liviu);
+        if(comparatie>0){
+            System.out.println("Media lui Alex este mai mare ca a lui Liviu");
+        }else if (comparatie<0){
+            System.out.println("Media lui Liviu este mai mare ca a lui Alex");
+        }else{
+            System.out.println("Cei doi au mediile egale");
+        }
+
+
+
+
+
+
     }
+
 }
--- Students/src/main/java/Student.java
+++ Students/src/main/java/Student.java
@@ -1,10 +1,13 @@
-public class Student extends Person implements Examinator,ExaminatorInfo,ExaminatorFizica {
-    int infoGrade;
-    int mathGrade;
-    int phizGrade;
-    boolean admisPoli;
-    boolean admisFizica;
-    boolean admisMateInfo;
+import java.io.Serializable;
+
+public class Student extends Person implements Examinator, ExaminatorInfo, ExaminatorFizica,Comparable<Student>, Serializable {
+    private int infoGrade;
+    private int mathGrade;
+    private int phizGrade;
+    private boolean admisPoli;
+    private boolean admisFizica;
+    private boolean admisMateInfo;
+    private int medie;
 
 
     public Student(String nume, String prenume, int mathGrade, int infoGrade, int phizGrade) {
@@ -18,7 +21,7 @@ public class Student extends Person implements Examinator,ExaminatorInfo,Examina
 
     public void computeAvragePoli() throws BadGradesExeption, GradesOutOfReachExeption {
         int medie = (this.infoGrade + this.mathGrade + this.phizGrade) / 3;
-        int medieF=(this.phizGrade+this.mathGrade)/2;
+        int medieF = (this.phizGrade + this.mathGrade) / 2;
         if (this.infoGrade <= 1 || this.infoGrade > 10 || this.mathGrade <= 1 || this.mathGrade > 10 || this.phizGrade <= 1 || this.phizGrade > 10) {
             throw new GradesOutOfReachExeption("Boss nu poti sa ii dai studentului note mai mari de 10 sau mai mici ca 1 ");
         } else if (medie < 5) {
@@ -32,32 +35,67 @@ public class Student extends Person implements Examinator,ExaminatorInfo,Examina
 
 
     @Override
-    public void computeAvrageFizica()throws BadGradesExeption, GradesOutOfReachExeption{
-        int medie=(this.phizGrade+this.mathGrade)/2;
-        if(this.infoGrade <= 1 || this.infoGrade > 10 || this.mathGrade <= 1 || this.mathGrade > 10 || this.phizGrade <= 1 || this.phizGrade > 10) {
+    public void computeAvrageFizica() throws BadGradesExeption, GradesOutOfReachExeption {
+        int medie = (this.phizGrade + this.mathGrade) / 2;
+        if (this.infoGrade <= 1 || this.infoGrade > 10 || this.mathGrade <= 1 || this.mathGrade > 10 || this.phizGrade <= 1 || this.phizGrade > 10) {
             throw new GradesOutOfReachExeption("Boss nu poti sa ii dai studentului note mai mari de 10 sau mai mici ca 1 ");
         } else if (medie < 5) {
             throw new BadGradesExeption("Studentul este repetent");
-        }else if (medie<8&&medie>5){
-            admisFizica=false;
-        }else{
-            admisFizica=true;
+        } else if (medie < 8 && medie > 5) {
+            admisFizica = false;
+        } else {
+            admisFizica = true;
         }
 
     }
 
     @Override
     public void computeAvrageInfo() throws BadGradesExeption, GradesOutOfReachExeption {
-        int medie=(this.infoGrade+this.mathGrade)/2;
-        if(this.infoGrade <= 1 || this.infoGrade > 10 || this.mathGrade <= 1 || this.mathGrade > 10 || this.phizGrade <= 1 || this.phizGrade > 10) {
+        int medie = (this.infoGrade + this.mathGrade) / 2;
+        if (this.infoGrade <= 1 || this.infoGrade > 10 || this.mathGrade <= 1 || this.mathGrade > 10 || this.phizGrade <= 1 || this.phizGrade > 10) {
             throw new GradesOutOfReachExeption("Boss nu poti sa ii dai studentului note mai mari de 10 sau mai mici ca 1 ");
         } else if (medie < 5) {
             throw new BadGradesExeption("Studentul este repetent");
-        }else if (medie<7||medie>5){
-            admisMateInfo=false;
-        }else{
-            admisMateInfo=true;
+        } else if (medie < 7 && medie > 5) {
+            admisMateInfo = false;
+        } else {
+            admisMateInfo = true;
         }
 
+    }
+    public int getInfoGrade(){
+        return infoGrade;
+    }
+
+    public int getMathGrade() {
+        return mathGrade;
+    }
+
+    public int getPhizGrade() {
+        return phizGrade;
+    }
+    public boolean getAdmisPoli(){
+        return admisPoli;
+    }
+
+    public boolean getAdmisFizica() {
+        return admisFizica;
+    }
+    public boolean getAdmisMateInfo(){
+        return admisMateInfo;
+    }
+    public int getMedie(){
+        this.medie=(this.mathGrade+this.phizGrade+this.infoGrade)/3;
+        return this.medie;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.medie>o.getMedie()){
+            return 1;
+        }else if(this.medie<o.getMedie()){
+            return -1;
+        }
+        return 0;
     }
 }
